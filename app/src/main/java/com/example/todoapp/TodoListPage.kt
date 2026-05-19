9c7cb46f2ec1205d186a11f7e157b5db633f427e
HAMDAOUI Slimane <h.sliman.2014@hotmail.com>
2026-05-19 21:14:35 +0100

    feat: add button and textfield UI

--- app/src/main/java/com/example/todoapp/TodoListPage.kt
+++ app/src/main/java/com/example/todoapp/TodoListPage.kt
@@ -1,35 +1,73 @@
 package com.example.todoapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todoapp.ui.Todo
 import com.example.todoapp.ui.getFakeTodos
 import java.text.SimpleDateFormat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun TodoListPage(){
     val todos = getFakeTodos()
+    var inputText by remember {
+        mutableStateOf("")
+        }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp)
+            .padding(5.dp)
+            .statusBarsPadding()
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = inputText,
+                onValueChange = {inputText = it},
+                label = { Text(text = "Enter todo")},
+                modifier = Modifier.weight(0.7f))
+            Button(
+                onClick = {},
+                ){
+                Text(
+                    text = "Add",
+                    color = Color.White,
+                    fontSize = 15.sp
+                )
+            }
+        }
         LazyColumn(
             content = {
                 itemsIndexed(todos){ index: Int, item: Todo ->
@@ -48,10 +86,12 @@ fun TodoItem(item: Todo) {
             .padding(8.dp)
             .clip(RoundedCornerShape(8.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
 
-    Column{
+    Column(
+        modifier = Modifier.weight(1f) ){
         Text(
             text = SimpleDateFormat("yyyy-MM-dd").format(item.createdAt),
             color = Color.LightGray,
@@ -63,5 +103,12 @@ fun TodoItem(item: Todo) {
             fontSize = 15.sp
         )
     }
+        IconButton(
+            onClick = {}) {
+            Icon(
+                painter = painterResource(id = R.drawable.outline_delete_24),
+                contentDescription = "delete",
+                tint = Color.White)
+        }
     }
 }
